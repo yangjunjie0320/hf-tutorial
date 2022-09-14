ab6ba3fb0d41809c4829db68fb5dd9f134383c17
Junjie Yang <yangjunjie0320@gmail.com>
2022-09-14 13:18:10 -0700

    update

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
     Reference: Szabo and Ostlund, 3.4.6. (p. 146, start from step 2)
 
     The SCF procedure is:
-        - Initialize the Fock matrix as hcore
+        - Initialize the density matrix by diagonalizing the core Hamiltonian
+        - Compute the Fock matrix
         - Diagonalize the Fock matrix
-        - Construct the occupied molecular orbitals
         - Compute the new density matrix
-        - Check convergence
+        - Compute the energy
+        - Compute the errors and check convergence
             - If converged, return the energy
             - If not converged, return to second step with new density matrix
 
@@ -29,17 +30,19 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
             The overlap matrix.
         eri : numpy.ndarray
             The two-electron repulsion integrals.
+        ene_nuc : float
+            The nuclear repulsion energy.
+        max_iter : int
+            The maximum number of SCF iterations.
         tol : float, optional
             The convergence tolerance, by default 1e-8.
-        max_iter : int, optional
-            The maximum number of iterations, by default 100.
     """
 
     nelec_alph, nelec_beta = nelecs
     assert nelec_alph == nelec_beta, "This code only supports closed-shell systems."
-
+    
     nao = hcore.shape[0]
-    nmo = nao
+    
     assert hcore.shape == (nao, nao)
     assert ovlp.shape  == (nao, nao)
     assert eri.shape   == (nao, nao, nao, nao)
@@ -50,31 +53,25 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
     is_converged = False
     is_max_iter  = False
 
-    ene_rhf = None
-    ene_cur = None
-    ene_old = None
-
     ene_err = 1.0
     dm_err  = 1.0
 
-    # Use the core Hamiltonian as the initial guess for the Fock matrix
-    fock   = hcore
-    dm_cur = None
-    dm_old = None
+    ene_rhf = None
+    ene_old = None
+    ene_cur = None
+
+    dm_old    = None # Initial density matrix
+    dm_cur    = None
 
     while not is_converged and not is_max_iter:
-        # Fill in the code here to perform the SCF iterations.
+        # Fill in the code here to perform SCF iterations.
 
-        ene_rhf = ene_cur
-        
-        if dm_old is not None:
+        # Compute the errors
+        if ene_old is not None:
             dm_err  = numpy.linalg.norm(dm_cur - dm_old)
             ene_err = abs(ene_cur - ene_old)
             print(f"SCF iteration {iter_scf:3d}, energy = {ene_rhf: 12.8f}, error = {ene_err: 6.4e}, {dm_err: 6.4e}")
 
-        dm_old  = dm_cur
-        ene_old = ene_cur
-        
         iter_scf += 1
         is_max_iter  = iter_scf >= max_iter
         is_converged = ene_err < tol and dm_err < tol
@@ -85,8 +82,7 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
         if ene_rhf is not None:
             print(f"SCF did not converge in {max_iter} iterations.")
         else:
-            ene_rhf = 0.0
-            print("SCF is not running.")
+            raise RuntimeError("SCF is not running, fill in the code in the main loop.")
 
     return ene_rhf
 
@@ -123,6 +119,7 @@ def main(inp: str) -> None:
         ene_rhf_ref = numpy.load(f"{int_dir}/ene_rhf.npy")
         ene_uhf_ref = numpy.load(f"{int_dir}/ene_uhf.npy")
 
+        # Implement the Hartree-Fock with SCF iterations 
         ene_rhf = solve_rhf(nelecs, hcore, ovlp, eri, tol=tol, max_iter=100, ene_nuc=ene_nuc)
 
         # This is the solution for RHF from Junjie, uncomment this to run it.
--- sol.py
+++ sol.py
@@ -11,7 +11,7 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
     assert nelec_alph == nelec_beta, "This code only supports closed-shell systems."
     
     nao = hcore.shape[0]
-    nmo = nao
+    
     assert hcore.shape == (nao, nao)
     assert ovlp.shape  == (nao, nao)
     assert eri.shape   == (nao, nao, nao, nao)
@@ -20,37 +20,45 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
     is_converged = False
     is_max_iter  = False
 
-    ene_rhf = 0.0
-    ene_cur = None
-    ene_old = None
-
     ene_err = 1.0
     dm_err  = 1.0
 
-    fock   = hcore
-    dm_cur = None
-    dm_old = None
+    ene_rhf = None
+    ene_old = None
+    ene_cur = None
 
+    nmo  = nao
     nocc = (nelec_alph + nelec_beta) // 2
     mo_occ = numpy.zeros(nmo, dtype=numpy.int)
     mo_occ[:nocc] = 2
     occ_list = numpy.where(mo_occ > 0)[0]
 
-    while not is_converged and not is_max_iter:
-        energy_mo, coeff_mo = eigh(fock, ovlp)
+    # Diagonalize the core Hamiltonian to get the initial guess for density matrix
+    energy_mo, coeff_mo = eigh(hcore, ovlp)
+    coeff_occ = coeff_mo[:, occ_list]
+    dm_old    = numpy.dot(coeff_occ, coeff_occ.T) * 2.0
+    dm_cur    = None
+    fock      = None
 
-        # Build the density matrix
-        coeff_occ = coeff_mo[:, occ_list]
-        dm_cur    = numpy.dot(coeff_occ, coeff_occ.T) * 2.0
-        
-        coul =   numpy.einsum("pqrs,rs->pq", eri, dm_cur)
-        exch = - numpy.einsum("prqs,rs->pq", eri, dm_cur) / 2.0
+    while not is_converged and not is_max_iter:
+        # Compute the Fock matrix
+        coul =   numpy.einsum("pqrs,rs->pq", eri, dm_old)
+        exch = - numpy.einsum("prqs,rs->pq", eri, dm_old) / 2.0
         fock = hcore + coul + exch
 
-        ene_cur = 0.5 * numpy.einsum("pq,pq->", hcore + fock, dm_cur) + ene_nuc
-        ene_rhf = ene_cur
+        # Diagonalize the Fock matrix
+        energy_mo, coeff_mo = eigh(fock, ovlp)
+
+        # Compute the new density matrix
+        coeff_occ = coeff_mo[:, occ_list]
+        dm_cur    = numpy.dot(coeff_occ, coeff_occ.T) * 2.0
+
+        # Compute the energy
+        ene_cur = 0.5 * numpy.einsum("pq,pq->", hcore + fock, dm_cur)
+        ene_rhf = ene_cur + ene_nuc
         
-        if dm_old is not None:
+        # Compute the errors
+        if ene_old is not None:
             dm_err  = numpy.linalg.norm(dm_cur - dm_old)
             ene_err = abs(ene_cur - ene_old)
             print(f"SCF iteration {iter_scf:3d}, energy = {ene_rhf: 12.8f}, error = {ene_err: 6.4e}, {dm_err: 6.4e}")
@@ -58,6 +66,7 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
         dm_old  = dm_cur
         ene_old = ene_cur
         
+        # Check convergence
         iter_scf += 1
         is_max_iter  = iter_scf >= max_iter
         is_converged = ene_err < tol and dm_err < tol
@@ -65,6 +74,10 @@ def solve_rhf(nelecs, hcore: numpy.ndarray, ovlp: numpy.ndarray, eri: numpy.ndar
     if is_converged:
         print(f"SCF converged in {iter_scf} iterations.")
     else:
-        print(f"SCF did not converge in {max_iter} iterations.")
+        if ene_rhf is not None:
+            print(f"SCF did not converge in {max_iter} iterations.")
+        else:
+            ene_rhf = 0.0
+            print("SCF is not running.")
 
     return ene_rhf
