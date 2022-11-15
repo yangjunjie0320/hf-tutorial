dc713c2fc916d36b96dc0146e6bd50d613972041
Junjie Yang <yangjunjie0320@gmail.com>
2022-11-14 20:52:55 -0800

    update uhf ref value

--- data/gen_data.py
+++ data/gen_data.py
@@ -1,6 +1,8 @@
 import os
 import numpy
 import pyscf
+from pyscf import scf
+from pyscf import gto
 
 def gen_data(inp):
     inp = inp.split('-')
@@ -58,28 +60,42 @@ def gen_data(inp):
         shutil.rmtree(int_dir)
         os.makedirs(int_dir)
 
-    mf = pyscf.scf.RHF(mol)
-    mf.max_cycle = 200
-    mf.conv_tol  = 1e-10
-    mf.kernel()
-    if mf.converged:
-        e_ref = mf.e_tot
+    rhf_obj = pyscf.scf.RHF(mol)
+    rhf_obj.max_cycle = 200
+    rhf_obj.conv_tol  = 1e-10
+    rhf_obj.kernel()
+    if rhf_obj.converged:
+        e_ref = rhf_obj.e_tot
         numpy.save(f"{int_dir}/ene_rhf.npy", e_ref)
     else:
         raise RuntimeError("SCF did not converge.")
     
-    mf = pyscf.scf.UHF(mol)
-    mf.max_cycle = 200
-    mf.conv_tol  = 1e-10
-    mf.kernel()
-    if mf.converged:
-        e_ref = mf.e_tot
+    uhf_obj = pyscf.scf.UHF(mol)
+    uhf_obj.max_cycle = 200
+    uhf_obj.conv_tol  = 1e-10
+    uhf_obj.kernel(rhf_obj.make_rdm1())
+
+    if uhf_obj.converged:
+        res = uhf_obj.stability(return_status=True)
+        mo_coeff = res[0]
+
+        stable_iter = 0
+        while not res[2] and stable_iter < 10:
+            uhf_obj.kernel(mo_coeff)
+            res = uhf_obj.stability(return_status=True)
+            mo_coeff = res[0]
+            stable_iter += 1
+        
+        assert uhf_obj.converged, "SCF did not converge."
+        assert res[2],            "SCF is not stable."
+
+        e_ref = uhf_obj.e_tot
         numpy.save(f"{int_dir}/ene_uhf.npy", e_ref)
     else:
         raise RuntimeError("SCF did not converge.")
 
     nelecs = mol.nelec
-    e_nuc  = mf.energy_nuc()
+    e_nuc  = rhf_obj.energy_nuc()
     hcore  = mol.intor('int1e_nuc')
     hcore += mol.intor('int1e_kin')
     ovlp   = mol.intor('int1e_ovlp')
